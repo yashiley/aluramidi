3a64fbc48df67928ce18cd40899ab6bd49368be2
yashiley <105359586+yashiley@users.noreply.github.com>
2023-11-21 11:26:04 -0300

    kkk

---
+++ main.js
@@ -0,0 +1,13 @@
+const aplausos = document.querySelector("#som_tecla_aplausos");
+
+function tocaSom(aplausos) {
+    alert(aplausos);
+    }
+    tocaSom("clap, clap, clap");
+
+    let comando = "tocar"
+    if(comando == "tocar"){
+        alert("Áudio pronto para tocar");
+     }else{
+        alert("O áudio  não está pronto");
+     }
